eafb461fe0239129a18b6a84c72e08056b516ed3
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-17 11:30:12 -0800

    Movie#create can now add new movie from external source into our database

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,28 +1,38 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
-
+  
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
     else
       data = Movie.all
     end
-
+    
     render status: :ok, json: data
   end
-
+  
   def show
     render(
       status: :ok,
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
-        )
       )
+    )
   end
-
+  
+  def create
+    init_order_count = 5
+    newMovie = Movie.new( title: params[:title], overview: params[:overview], release_date: params[:release_date], inventory: init_order_count, image_url: params[:image_url], external_id: params[:external_id])
+    if newMovie.save
+      puts "SUCCESSS!"
+    else  
+      puts "DAMMIT"
+    end
+  end
+  
   private
-
+  
   def require_movie
     @movie = Movie.find_by(title: params[:title])
     unless @movie
