7a8792533b5153473abe27a72369788600c8e6bc
Michaela Morrow <morrow.michaela@gmail.com>
2019-12-17 16:50:02 -0800

    Attempted adding create to movie controller, not working yet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,19 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
 
+  def create
+    movie = Movie.new(movie_params)
+    movie.inventory = 10
+
+    if movie.save
+      render json: movie.as_json, status: :ok
+      return
+    else
+      render json: {errors: movie.errors.messages}, status: :not_acceptable
+      return
+    end
+  end
+
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
@@ -28,5 +41,9 @@ class MoviesController < ApplicationController
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
+  end
+
+  def movie_params
+    params.permit(:title, :overview, :release_date, :image_url, :external_id)
   end
 end
