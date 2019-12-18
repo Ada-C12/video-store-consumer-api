ec00ca7d29399f773070282f406151435997dce2
Eve Le <nguyenthao.lee@gmail.com>
2019-12-17 16:25:03 -0800

    Added movies#create action for Video Store API

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,28 +1,50 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
-
+  
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
     else
       data = Movie.all
     end
-
+    
     render status: :ok, json: data
   end
-
+  
   def show
     render(
       status: :ok,
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
+      )
+    )
+  end
+  
+  
+  def create
+    new_movie = Movie.new(movie_params)
+    
+    if new_movie.save
+      render(
+        status: :ok,
+        json: new_movie.as_json(
+          only: [:title, :overview, :release_date, :external_id, :image_url]
         )
       )
+    else
+      render json: {
+        ok: false,
+        errors: new_movie.errors.messages
+        }, status: :bad_request
+    end
   end
-
+    
   private
-
+  def movie_params
+    return params.permit(:title, :overview, :release_date, :external_id, :image_url)
+  end
+  
   def require_movie
     @movie = Movie.find_by(title: params[:title])
     unless @movie
@@ -30,3 +52,4 @@ class MoviesController < ApplicationController
     end
   end
 end
+  
