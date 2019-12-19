cb7e6f667b310f3542ff10565c200f25b9d0bee8
Brianna Kemp <kemp.bri@gmail.com>
2019-12-18 16:05:38 -0800

    Added create method for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,28 +1,38 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
-
+  
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
     else
       data = Movie.all
     end
-
+    
     render status: :ok, json: data
   end
-
+  
   def show
     render(
       status: :ok,
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
-        )
       )
+    )
   end
-
+  
+  def create
+    movie = MovieWrapper.construct_movie(params)
+    
+    if movie.save
+      render status: :ok, json: {}
+    else 
+      render status: :bad_request, json: {errors: movie.errors.messages}
+    end
+  end
+  
   private
-
+  
   def require_movie
     @movie = Movie.find_by(title: params[:title])
     unless @movie
