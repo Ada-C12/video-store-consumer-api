e6094fdd4e8112d39bb725d2bb7ec93e52551193
Hallie <idhallie@gmail.com>
2019-12-19 14:48:41 -0800

    Added seeds for rentals.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,15 @@ end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie_data|
   sleep(0.1)                    # Sleep to avoid hammering the API.
-
+  
   movies = MovieWrapper.search(movie_data["title"])
   ap "#{movie_data['title']} Added to the library!"
   movies.first.inventory = movie_data['inventory']
   movies.first.save unless movies.empty?
+end
+
+rental_data = JSON.parse(File.read('db/seeds/rentals.json'))
+
+rental_data.each do |rental|
+  Rental.new(rental).save(validate: false)
 end
