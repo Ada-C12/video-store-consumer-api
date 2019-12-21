7a8fbcbf82d71f7e8d768e8ca427f0a4c45a340f
Morgan Schuler <morganlschuler@gmail.com>
2019-12-20 22:54:55 -0800

    finished styling

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,18 +1,18 @@
 class RentalsController < ApplicationController
   before_action :require_movie, only: [:check_out, :check_in]
   before_action :require_customer, only: [:check_out, :check_in]
-
+  
   # TODO: make sure that wave 2 works all the way
   def check_out
     rental = Rental.new(movie: @movie, customer: @customer, due_date: params[:due_date])
-
+    
     if rental.save
-      render status: :ok, json: {}
+      render status: :ok, json: { id: rental.id }
     else
       render status: :bad_request, json: { errors: rental.errors.messages }
     end
   end
-
+  
   def check_in
     rental = Rental.first_outstanding(@movie, @customer)
     unless rental
@@ -29,22 +29,22 @@ class RentalsController < ApplicationController
       render status: :bad_request, json: { errors: rental.errors.messages }
     end
   end
-
+  
   def overdue
     rentals = Rental.overdue.map do |rental|
       {
-          title: rental.movie.title,
-          customer_id: rental.customer_id,
-          name: rental.customer.name,
-          postal_code: rental.customer.postal_code,
-          checkout_date: rental.checkout_date,
-          due_date: rental.due_date
+        title: rental.movie.title,
+        customer_id: rental.customer_id,
+        name: rental.customer.name,
+        postal_code: rental.customer.postal_code,
+        checkout_date: rental.checkout_date,
+        due_date: rental.due_date
       }
     end
     render status: :ok, json: rentals
   end
-
-private
+  
+  private
   # TODO: make error payloads arrays
   def require_movie
     @movie = Movie.find_by title: params[:title]
@@ -52,7 +52,7 @@ private
       render status: :not_found, json: { errors: { title: ["No movie with title #{params[:title]}"] } }
     end
   end
-
+  
   def require_customer
     @customer = Customer.find_by id: params[:customer_id]
     unless @customer
